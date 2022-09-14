3961f958c33736bbbb9e5dbd401cb291f2d59097
k-p4 <69355540+k-p4@users.noreply.github.com>
2022-09-14 09:43:51 -0500

    cleaned up and troubleshooting independence_test

--- scripts/00_long_data_permtest_1.R
+++ scripts/00_long_data_permtest_1.R
@@ -47,7 +47,7 @@ df7 <- df6 %>%
         filter(!grepl('K4me3K27un', ptm_combination))
 
 # KS test for distributions and normality
-df8 <-df7 %>% 
+df8 <- df7 %>%
         group_by(ptm_combination, treatment) %>% 
         nest() %>% 
         pivot_wider(names_from = treatment, values_from = data) %>% 
@@ -56,11 +56,11 @@ df8 <-df7 %>%
                 ks_test = map2(ctrl, but, ~{ks.test(.x$interplay_score, .y$interplay_score) %>% tidy()}),
                 ctrl = map(ctrl, nrow),
                 but = map(but, nrow)
-        ) %>% 
+        ) %>%
         unnest(cols = c(ctrl, but, ks_test))
 
 # Wilcox test
-df9 <-df7 %>% 
+df9 <- df7 %>%
         group_by(ptm_combination, treatment) %>% 
         nest() %>% 
         pivot_wider(names_from = treatment, values_from = data) %>% 
@@ -73,8 +73,7 @@ df9 <-df7 %>%
         unnest(cols = c(ctrl, but, wilcox_test))
 
 # Welch t.test
-df10 <-df7 %>% 
-
+df10 <- df7 %>%
         group_by(ptm_combination, treatment) %>% 
         nest() %>% 
         pivot_wider(names_from = treatment, values_from = data) %>% 
@@ -88,3 +87,17 @@ df10 <-df7 %>%
 
 
 # permutation test and writing permissions to be solved
+
+
+# # coin permutation test of independece
+# df11 <- df7 %>%
+#         group_by(ptm_combination, treatment) %>% 
+#         nest() %>% 
+#         pivot_wider(names_from = treatment, values_from = data) %>% 
+#         # depreciated: spread(key = treatment, value = data) %>% 
+#         mutate(
+#                 independence_test = map2(ctrl, but, ~{independence_test(.x$interplay_score ~ .y$interplay_score) %>% tidy()}),
+#                 ctrl = map(ctrl, nrow),
+#                 but = map(but, nrow)
+#         ) %>% 
+#         unnest(cols = c(ctrl, but, independence_test))
